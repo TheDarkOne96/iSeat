a9971eabd4e21c8745b4a36b475f424cf40ae644
kingmkm <mk4ever2009@live.com>
2017-11-08 22:36:05 +0400

    complete the senario
    added the senario of car off/on
    wrote where the functions should go
    wrote what sensors to read
    wrote the logical procedure
    note: add the functions on next change

--- main/main.ino
+++ main/main.ino
@@ -7,17 +7,19 @@ const int led_blue=6;//blue led
 const int sms1=7;
 const int sms2=8;
 const int micro=10;
-const int ODE=11;
+const int OBD=11;
+
+
 void setup() {
   Serial.begin(9600);
   pinMode(motion, INPUT);
   pinMode(button_seat_1, INPUT);
-  pinMode(button_seat_1, INPUT);
+  pinMode(button_seat_2, INPUT);
   pinMode(led_red, OUTPUT);
   pinMode(led_yellow, OUTPUT);
   pinMode(led_blue, OUTPUT);
   pinMode(led_blue, OUTPUT);
-  pinMode(ODE, OUTPUT);
+  pinMode(OBD, INPUT);
   digitalWrite(motion, LOW);
   
   Serial.print("calibrating sensor ");
@@ -32,19 +34,45 @@ void setup() {
 int button0_date=0;//save the value of ODE
 int button1_data=0;//save the value read from button 1
 int button2_data=0;//save the value read from button 2
+int read_micro=0;//save the value recieved from the microphone
+int read_temp=0;//save the value recieved from the tempruture sensor
+int checks;// when the car detect a child it will try varify if the kid is in the car
+
+
 void loop() {
   button1_data=digitalRead(button_seat_1);
   if(button1_data==1){//that means the seat is in the car
-      button1_data=digitalRead(ODE);
+      button1_data=digitalRead(OBD);
       if(button0_data==0){//that means the car is off
-        
+        // read_micro=function_micro();
+        // read_motion=function_motion();
+        // button2_data=digitalRead(button_seat_2);
+        //if((read_motion>=x)||(read_micro>=y)||(button2_data==1)){
+            //checks=0;
+            //for(int i=0;i<24;i++){
+                 // read_micro=function_micro();
+                 // read_temp=function_micro();
+                 // button2_data=digitalRead(button_seat_2);
+                 //if((read_temp>=x)||(read_micro>=y)||(button2_data==1)){checks++;}
+                 //delay(10000)//delay 10 seconds
+            //}
+            //if(checks>7){
+            //read_gas=function_gas();
+            //read_temp=function_temp();
+            //if((read_temp>=x)||(read_gas>=y)){ //if values form the sensor are greater than set values child is in danger
+                // function_emargancy();}
+            //else{
+                  //function_normal();
+                  //}
+      //}
   }
-      else {delay(240,00);}//it will wait 4 mintues before checking on the car state
+      else {//excute senario if 
+        // read_gas=function_gas();
+        // read_temp=function_micro();
+        //if((read_temp>=x)||(read_gas>=y)){ //if values form the sensor are greater than set values child is in danger
+        // function_emargancy();}
+      }
   }
   else {delay(240,00);}//it will wait 4 mintues before checking on the car state
   }
-  
-
- 
-
 }
